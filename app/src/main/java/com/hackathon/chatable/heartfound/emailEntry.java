2cfe3efe9283220b86c3275322a889fdd0525836
mitchanderso <mitch.anderso@gmail.com>
2015-07-22 22:51:37 +0930

    big changes pt2

--- app/src/main/java/com/hackathon/chatable/heartfound/emailEntry.java
+++ app/src/main/java/com/hackathon/chatable/heartfound/emailEntry.java
@@ -4,13 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 
 
 public class emailEntry extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_email_entry);
     }
 
